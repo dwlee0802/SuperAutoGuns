c634113c311972a6d41785028c85b56a84cde2f0
unknown <dwlee0802@gmail.com>
2024-03-17 23:01:22 +0900

    Changed unit card and slot sizes.

--- Scenes/UnitMatrixEditor.gd
+++ Scenes/UnitMatrixEditor.gd
@@ -2,11 +2,9 @@ extends VBoxContainer
 class_name UnitMatrixEditor
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func UpdateUnitMatrix():
+	pass
+	
+	
+func GenerateReinforcementOptions():
 	pass
