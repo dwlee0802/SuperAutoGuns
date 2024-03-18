6e50686246e2b0f9dba870dbc62ed898d0f74dfc
unknown <dwlee0802@gmail.com>
2024-03-18 19:55:33 +0900

    Placeholder methods for drag and dropping.

--- Scripts/UnitCard.gd
+++ Scripts/UnitCard.gd
@@ -7,3 +7,12 @@ var unit: Unit
 func SetUnit(_unit: Unit):
 	unit = _unit
 	$Name.text = unit.data.name
+
+
+func _get_drag_data(_at_position: Vector2) -> Variant:
+	#set_drag_preview(make_drag_preview())
+	return self
+	
+
+func make_drag_preview() -> TextureRect:
+	pass
