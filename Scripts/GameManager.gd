991314ce1803e643af1d7d7ccfd9dedf92d73a1c
unknown <dwlee0802@gmail.com>
2024-05-29 12:23:19 +0900

    Changed turn time back to normal.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -102,7 +102,7 @@ static var enemyColor: Color
 @export var playerColorOverride: Color = Color.ROYAL_BLUE
 @export var enemyColorOverride: Color = Color.DARK_RED
 
-@export var turnTime: float = 3
+@export var turnTime: float = 30
 
 static var fundsGraph
 
