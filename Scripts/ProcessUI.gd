9f073e7fe8cbf6970ee594b3d27f18129e79259c
unknown <dwlee0802@gmail.com>
2024-06-24 20:20:43 +0900

    Started making updated capture status ui.

--- Scripts/ProcessUI.gd
+++ Scripts/ProcessUI.gd
@@ -1,7 +1,10 @@
 extends Control
 class_name CaptureStatusUI
 
-@onready var container: HBoxContainer = $HBoxContainer
+@onready var leftContainer: HBoxContainer = $LeftCaptured
+@onready var rightContainer: HBoxContainer = $RightCaptured
+
+@onready var contestedContainer: HBoxContainer = $Contested
 
 var sectorIconScene = load("res://Scenes/sector_icon.tscn")
 
@@ -11,21 +14,36 @@ func _ready():
 	ReloadUI(5)
 
 
-func SetTotalSectorCount(count: int):
-	for i in range(max(count, container.get_child_count())):
-		if i < container.get_child_count():
-			if i >= count:
-				container.get_child(i).queue_free()
-		else:
-			var newIcon = sectorIconScene.instantiate()
-			container.add_child(newIcon)
+func SetColors(leftColor: Color, rightColor: Color):
+	pass
 	
-	container.get_child(0).get_node("Label").visible = true
-	container.get_child(-1).get_node("Label").visible = true
+	
+func SetTotalSectorCount(count: int):
+	if count % 2 == 1:
+		push_warning("Total sector count shouldn't be an odd number.")
+	
+	# remove preexisting stuff
+	for i in range(leftContainer.get_child_count()):
+		leftContainer.get_child(i).queue_free()
+	for i in range(rightContainer.get_child_count()):
+		rightContainer.get_child(i).queue_free()
+	
+	# assume count is always even
+	# add normal squares except contested squares
+	for i in range(count/2 - 1):
+		var newIcon = sectorIconScene.instantiate()
+		leftContainer.add_child(newIcon)
+		rightContainer.add_child(newIcon)
+	
+	# capital indicator
+	leftContainer.get_child(0).get_node("Label").visible = true
+	rightContainer.get_child(-1).get_node("Label").visible = true
 	
 	
 func ReloadUI(playerCapturedSectors: int):
-	for i in range(container.get_child_count()):
+	var notCotestedPlayerCount: int = playerCapturedSectors - 1
+	
+	for i in range(leftContainer.get_child_count()):
 		if i < playerCapturedSectors:
 			container.get_child(i).self_modulate = Color.ROYAL_BLUE
 		else:
